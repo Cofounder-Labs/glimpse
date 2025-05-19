4fad242fb0937520471968375d88532048e733af
ritanshudokania <ritanshudokania@gmail.com>
2025-05-19 15:03:29 -0700

    Show video in frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*', // Proxy API requests to Backend
+      },
+      {
+        source: '/recordings/:path*',
+        destination: 'http://127.0.0.1:8000/recordings/:path*', // Proxy recording requests to Backend
+      },
+    ];
+  },
 }
 
 export default nextConfig
